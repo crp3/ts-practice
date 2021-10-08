c936ef34e9239107b2d4cd381ea7b5149b010f7e
Caio Rocha <caiorochapstrlrd@gmail.com>
2021-10-08 13:51:02 -0300

    feat: remove dist folder

--- wishlist/dist/app/controllers/health.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res, next) => {
-    res.send(200, 'Service is up');
-    return next();
-};
-//# sourceMappingURL=health.js.map
--- wishlist/dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({
-    PORT: process.env.PORT || '8080',
-    SERVICE: process.env.SERVICE || 'wishlist',
-    LOG_LEVEL: process.env.LOG_LEVEL || 'debug'
-});
-//# sourceMappingURL=index.js.map
