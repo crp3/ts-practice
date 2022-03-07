be706657dfdd74a389dab36df570fcd0dec7ec74
Caio Rocha <caiorochapstrlrd@gmail.com>
2022-03-07 16:43:52 -0300

    chore: modify path to file

--- streams/src/fs_server/index.ts
+++ streams/src/fs_server/index.ts
@@ -2,6 +2,6 @@ import http from 'http'
 import { createReadStream } from 'fs'
 
 http.createServer((_, res) => {
-  createReadStream("big.file")
+  createReadStream("src/fs_server/big.file")
   .pipe(res)
 }).listen(3000, () => console.log('running at 3000'))
