9b08e737e2d7ce7e9f20ec0cddf022c7d431bd09
Caio Rocha <caiorochapstrlrd@gmail.com>
2022-03-18 00:13:43 -0300

    feat: added entire first module of course

--- nestjs/nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs/nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,7 +1,52 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  public getTasks(
+    @Query() filterDTO: GetTasksFilterDTO,
+  ): Task[]{
+    if (Object.keys(filterDTO).length) {
+      return this.tasksService.getTasksWithFilters(filterDTO);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get(':taskId')
+  public getTaskById(@Param('taskId') taskId: string): Task {
+    return this.tasksService.getTaskById(taskId);
+  }
+
+  @Delete(':taskId')
+  public deleteTaskById(@Param('taskId') taskId: string): void {
+    this.tasksService.deleteTaskById(taskId);
+  }
+
+  @Post()
+  public createTask(
+    @Body() createTaskDTO: CreateTaskDTO,
+  ): Task {
+    return this.tasksService.createTask(
+      createTaskDTO,
+    );
+  }
+
+  @Patch(':taskId/status')
+  public updateTaskStatus(
+    @Param('taskId') taskId: string,
+    @Body('new_status') newStatus: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(
+      taskId, newStatus
+    )
+  }
+
+  
 }
--- nestjs/nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs/nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,4 +1,73 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: Task[] = [];
+
+    public getAllTasks(): Task[]{
+        return this.tasks;
+    }
+
+    public deleteTaskById(taskId: string): void{
+        this.tasks = this.tasks.filter(
+            (task: Task) => 
+                task.id !== taskId
+        );
+    }
+
+    public createTask(createTaskDTO: CreateTaskDTO): Task {
+        const { title, description } = createTaskDTO;
+
+        const task: Task = {
+            id: uuid(),
+            title, 
+            description,
+            status: TaskStatus.OPEN,
+        }
+
+        this.tasks.push(task);
+        return task;
+    }
+
+    public getTasksWithFilters(filterDto: GetTasksFilterDTO): Task[] {
+        const { status, search } = filterDto;
+
+        let tasks = this.getAllTasks();
+
+        tasks = status ? 
+            tasks.filter(
+                (task: Task) => 
+                    task.status === status
+                ): tasks;
+
+        
+        tasks = search ?
+            tasks.filter(
+                (task: Task) =>
+                    task.title.includes(search) || task.description.includes(search)
+                ): tasks;
+
+        return tasks;
+    }
+
+    public updateTaskStatus(taskId: string, newStatus: TaskStatus): Task {
+        const taskIndex = this.tasks.findIndex(
+            (task: Task) =>
+                task.id === taskId
+        );
+
+        this.tasks[taskIndex].status = newStatus;
+        return this.tasks[taskIndex];
+    }
+
+    public getTaskById(taskId: string): Task {
+        return this.tasks.find(
+            (task: Task) =>
+                task.id === taskId
+            );
+    }
+}
